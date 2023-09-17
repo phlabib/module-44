ef1db190dd15346654cc88dcbc4ae02771d87e6d
phlabib <ph.labib@gmail.com>
2023-09-17 23:21:26 +0600

    module 44

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <DaisyNav></DaisyNav> */}
+    <NavBar></NavBar>
+       <h1 className='text-7xl'>Vite + React</h1>
+   
+    </>
+  )
+}
+
+export default App
