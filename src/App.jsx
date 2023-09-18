4900675d2eb451cdde293ee2eb2f04f6b3f03c9a
phlabib <ph.labib@gmail.com>
2023-09-19 02:03:43 +0600

    Add lineChart and BarChart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 
+
 import './App.css'
+import BarChart from './components/BarChart/BarChart'
+import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
@@ -13,6 +16,8 @@ function App() {
     {/* <DaisyNav></DaisyNav> */}
        <h1 className='text-7xl'>Vite + React</h1>
     <PriceOptions></PriceOptions>
+    <LineChart></LineChart>
+    <BarChart></BarChart>
     </>
   )
 }
---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,113 @@
+
+import { LineChart as LChart, Line, XAxis, YAxis } from 'recharts';
+const LineChart = () => {
+
+    const subjectMarksData = [
+        {
+          "ID": "S001",
+          "Name": "Alice",
+          "Math": 75,
+          "Science": 88,
+          "English": 92,
+          "History": 79,
+          "Art": 87
+        },
+        {
+          "ID": "S002",
+          "Name": "Bob",
+          "Math": 82,
+          "Science": 78,
+          "English": 85,
+          "History": 91,
+          "Art": 76
+        },
+        {
+          "ID": "S003",
+          "Name": "Charlie",
+          "Math": 65,
+          "Science": 72,
+          "English": 68,
+          "History": 73,
+          "Art": 70
+        },
+        {
+          "ID": "S004",
+          "Name": "David",
+          "Math": 91,
+          "Science": 95,
+          "English": 88,
+          "History": 93,
+          "Art": 90
+        },
+        {
+          "ID": "S005",
+          "Name": "Eve",
+          "Math": 78,
+          "Science": 84,
+          "English": 79,
+          "History": 82,
+          "Art": 77
+        },
+        {
+          "ID": "S006",
+          "Name": "Frank",
+          "Math": 89,
+          "Science": 92,
+          "English": 90,
+          "History": 88,
+          "Art": 91
+        },
+        {
+          "ID": "S007",
+          "Name": "Grace",
+          "Math": 72,
+          "Science": 76,
+          "English": 70,
+          "History": 75,
+          "Art": 74
+        },
+        {
+          "ID": "S008",
+          "Name": "Hannah",
+          "Math": 95,
+          "Science": 98,
+          "English": 93,
+          "History": 96,
+          "Art": 94
+        },
+        {
+          "ID": "S009",
+          "Name": "Isaac",
+          "Math": 84,
+          "Science": 87,
+          "English": 82,
+          "History": 85,
+          "Art": 80
+        },
+        {
+          "ID": "S010",
+          "Name": "Jane",
+          "Math": 70,
+          "Science": 72,
+          "English": 68,
+          "History": 71,
+          "Art": 69
+        }
+      ];
+      
+
+
+    return (
+        <div>
+            <LChart width={800} height={400} data={subjectMarksData}>
+                <XAxis dataKey={'Name'}></XAxis>
+                <YAxis></YAxis>
+                <Line dataKey="Math" stroke='red'> </Line>
+                <Line dataKey={'History'} stroke='green'></Line>
+            </LChart>
+        </div>
+       
+    );
+};
+
+export default LineChart;
